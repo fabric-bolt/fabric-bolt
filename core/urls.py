b933bfaff38c5d151ab76d240d10203c8c289f83
Nathaniel Pardington <npardington@gmail.com>
2013-09-27 20:11:40 -0400

    Hook up the project

---
+++ core/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+#Serve the static files from django
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
