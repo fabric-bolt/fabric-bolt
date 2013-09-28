80891c5a6f255ceea1865531941b9644b22d9994
Nathaniel Pardington <npardington@gmail.com>
2013-09-27 22:17:26 -0400

    Add some generic views

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,24 @@
-from django.views.generic import CreateView
+from django.core.exceptions import ImproperlyConfigured
+from django.views.generic import CreateView, UpdateView
 
 import models
 
 
 class CreateProject(CreateView):
+    model = models.Project
+
+    def get_success_url(self):
+        if self.success_url:
+            url = self.success_url % self.object.__dict__
+        else:
+            try:
+                url = self.object.get_absolute_url()
+            except AttributeError:
+                raise ImproperlyConfigured(
+                    "No URL to redirect to.  Either provide a url or define"
+                    " a get_absolute_url method on the Model.")
+        return url
+
+
+class EditProject(UpdateView):
     model = models.Project
