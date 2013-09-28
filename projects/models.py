94c2ce136dc03d65bd8a4d13aa95f7e4ad590df9
Nathaniel Pardington <npardington@gmail.com>
2013-09-27 21:37:08 -0400

    Add project app with initial migration

---
+++ projects/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from core.mixins.models import TrackingFields
+
+
+class ProjectType(TrackingFields):
+    name = models.CharField(max_length=255)
+
+
+class Project(TrackingFields):
+    """Model for a project (pretty obvious)
+
+    Keeps track of the stages and general configurations for deployments"""
+
+    name = models.CharField(max_length=255)
+
+    type = models.ForeignKey(ProjectType, blank=True, null=True)
+
+    description = models.TextField(blank=True, null=True)
+
+    # Misc information for a project
+    number_of_deployments = models.IntegerField(default=0)
