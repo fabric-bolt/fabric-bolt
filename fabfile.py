2806517a37791b2b72e534a037bbc914cf33ba7c
Nathaniel Pardington <npardington@gmail.com>
2013-09-29 19:26:47 -0400

    Add some info in the fab tasks

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import *
 
 
 def update():
+    """Requires code_root env variable. Does a git pull and restarts the web server"""
     require('code_root')
 
     git_pull()
@@ -10,28 +11,33 @@ def update():
 
 
 def git_pull():
+    """Does a git stash then a git pull on the project"""
     run('cd %s; git stash; git pull' % (env.code_root))
 
 
 def restart_web_server():
-    "Restart the web server"
+    """Restart the web server"""
     run('%s/apache2/bin/restart' % env.code_root_parent)
 
 
 def migrate():
+    """Runs python manage.py migrate"""
     run('cd %s; python manage.py migrate --settings=%s' % (env.code_root, env.settings_file))
 
 
 def collect_static():
+    """Runs python manage.py collect_static --noinput"""
     run('cd %s; python manage.py collectstatic --settings=%s --noinput' % (env.code_root, env.settings_file))
 
 
 def pip_install():
+    """Runs pip install -r requirements/frozen.txt (for example site)"""
     run('cd %s; pip install -r requirements/frozen.txt' % (env.code_root))
 
 
 def publish_changes():
-    update()
+    """Runs these functions in order (git_pull, pip_install, migrate, collect_static, restart_web_server)"""
+    git_pull()
     pip_install()
     migrate()
     collect_static()
