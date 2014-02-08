c42bf43b0c663bab30c16d186ec82c728b289dc3
paperreduction <danddietz@gmail.com>
2014-02-08 10:54:35 -0500

    Updated the dashboard view to run raw sql (for postgres) to generate the deployment history data. This resolves a performance issue for postgres users due to a nested for loop call to the ORM.

--- src/fabric_bolt/core/views.py
+++ src/fabric_bolt/core/views.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 import json
 
+from django.db import connection
 from django.views.generic import TemplateView
 from django.db.models.aggregates import Count
 
@@ -14,20 +15,87 @@ class Dashboard(TemplateView):
 
         context = super(Dashboard, self).get_context_data(**kwargs)
 
+        # Get the deployments and projects and bail if we don't have any
         deploys = Deployment.objects.order_by('date_created')
-
-        if deploys.count() == 0:
+        if not deploys.exists():
             return context
 
-        start_date = deploys[0].date_created
-        end_date = deploys[deploys.count()-1].date_created
+        projects = Project.objects.all()
+        if not projects.exists():
+            return context
 
+        # Deployment Stats Data
+        # Build pie chart data to show % projects deployed successfully
+        items = [[item['status'], item['count']] for item in Deployment.objects.order_by('status').values('status').annotate(count=Count('id'))]
+        context['pie_chart_data'] = json.dumps(items)
+
+        # Deployment History Data
+        # Get all projects with the days they were deployed and the count of the deploys on each day
         chart_data = []
 
-        projects = Project.objects.all()
+        # If we're using postgres do this with one query, otherwise pound the db with the ORM.
+        if connection.vendor == 'postgresql':
+            # I couldn't figure out how to do this in the ORM with a Group By... here's a postgres version.
+            # If you're smarter than me, and can fix this, please do.
+            cursor = connection.cursor()
+            cursor.execute("""
+            SELECT project.name as project_name
+            , date_trunc('day', deployment.date_created) as deploy_date
+            , COUNT(date_trunc('day', deployment.date_created)) as deploy_count
+            FROM "projects_project" as project
+            LEFT JOIN "projects_stage" as stage ON ( project."id" = stage."project_id" )
+            LEFT JOIN "projects_deployment" as deployment ON ( stage."id" = deployment."stage_id" )
+            GROUP BY project.name, date_trunc('day', deployment.date_created)
+            ORDER BY date_trunc('day', deployment.date_created), project.name
+            """)
 
-        if projects.count():
+            rows = cursor.fetchall()
 
+            # Get a distinct list of projects from our results
+            project_names = []
+            previous_project = ''
+            for row in rows:
+                if row[0] == previous_project:
+                    continue
+                previous_project = row[0]
+                project_names.append(row[0])
+
+            # Convert the sql results into a dictionary indexed by project and date (this makes the next step easier)
+            project_deployment_data = {}
+            for project in project_names:
+                project_deployment_data[project] = {}
+                for row in rows:
+                    if project == row[0] and row[1] is not None:
+                        project_deployment_data[project][row[1].strftime('%m/%d')] = row[2]
+
+            # Build the google chart data using the project and date dictionary
+            # We need an array for each day that has the deployment counts for each project
+            deploy_day = None
+            for row in rows:
+                # If we have a project w/ no deployments, keep going
+                if row[1] is None:
+                    continue
+
+                # If we already processed this day, keep going
+                if row[1] == deploy_day:
+                    continue
+
+                # Process this day for each project
+                deploy_day = row[1].strftime('%m/%d')
+                daily_counts = [deploy_day]
+                for project in project_names:
+                    daily_counts.append(project_deployment_data[project].get(deploy_day, 0))
+
+                chart_data.append(daily_counts)
+
+            chart_data = [['Day'] + project_names] + chart_data
+
+        else:
+            # Get the date range for all the deployments ever done
+            start_date = deploys[0].date_created
+            end_date = deploys[deploys.count()-1].date_created
+
+            # Step through each day and create an array of deployment counts from each project
             for day in range(-1, (end_date.date() - start_date.date()).days + 1):
                 date = start_date + timedelta(days=day)
                 if day == -1:
@@ -41,14 +109,11 @@ class Dashboard(TemplateView):
                         continue
 
                     range_ = (date.date(), date.date() + timedelta(days=1))
-                    data.append(Deployment.objects.filter(stage__project_id=project.pk, date_created__range=range_).count())
+                    # Nested ORM Call... not good.
+                    data.append(Deployment.objects.select_related('stage').filter(stage__project_id=project.pk, date_created__range=range_).count())
 
                 chart_data.append(data)
 
-            context['chart_data'] = json.dumps(chart_data)
-
-        items = [[item['status'], item['count']] for item in Deployment.objects.order_by('status').values('status').annotate(count=Count('id'))]
-
-        context['pie_chart_data'] = json.dumps(items)
+        context['chart_data'] = json.dumps(chart_data)
 
         return context
