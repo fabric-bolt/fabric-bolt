e38f2eb4c28b05834d7e382c70b2ac35e9c7e0a0
Jared Proffitt <jproffitt@worthwhile.com>
2014-07-23 14:37:40 -0500

    Trying to fix travis bug

--- fabric_bolt/manage.py
+++ fabric_bolt/manage.py
@@ -5,14 +5,14 @@ import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fabric_bolt.core.settings.local')
+    sys.path.append(os.getcwd())
+
     from django.conf import settings
 
     if getattr(settings, 'SOCKETIO_ENABLED', False):
         from gevent import monkey
         monkey.patch_all()
-
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fabric_bolt.core.settings.local')
-    sys.path.append(os.getcwd())
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
