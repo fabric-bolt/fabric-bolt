84f1cc1076ac0dfda47f5a5670368bc67667652e
Jared Proffitt <jproffitt@worthwhile.com>
2014-09-05 17:12:01 -0500

    Upgrading everything else while i'm at it

--- setup.py
+++ setup.py
@@ -5,35 +5,34 @@ from setuptools import setup, find_packages
 setup_requires = []
 
 install_requires = [
-    'pillow>=2.5.1,<2.6',
+    'django>=1.7,<1.8',
+    'pillow>=2.5.3,<2.6',
     'django-stronghold>=0.2.6,<0.3',
     'django-crispy-forms>=1.4.0,<1.5',
-    'django-custom-user>=0.4,<0.5',
+    'django-authtools>=1.0.0,<1.1',
     'django-tables2>=0.15.0,<0.16',
     'django-braces>=1.4,<1.5',
     'django-sekizai>=0.7,<0.8',
-    'django-grappelli>=2.5.1,<2.6',
-    'fabric>=1.9.1,<1.10',
+    'fabric>=1.10.0,<1.11',
     'logan>=0.5.10,<0.6',
-    'GitPython',
     'django-bootstrap-form>=3.1,<3.2',
     'croniter>=0.3.5,<0.4',
+    'GitPython',
     'gevent-socketio>=0.3.6,<0.4',
+    'requests>=2.4.0,<2.5',
     'virtualenv>=1.11.6,<1.12',
-    'requests',
-    'django-authtools>=1.0.0',
 ]
 
 dev_requires = [
-    'django_debug_toolbar>=1.2.1,<1.3',
-    'django-grappelli>=2.5.3,<2.6',
+    'django-debug-toolbar>=1.2.1,<1.3',
     'django-debug-toolbar-template-timings>=0.6.4,<0.7',
     'mock>=1.0.1,<1.1',
+    'django-grappelli>=2.5.3,<2.6',
     'model_mommy>=1.2.1,<1.3'
 ]
 
 postgres_requires = [
-    'psycopg2>=2.5.3,<2.6',
+    'psycopg2>=2.5.4,<2.6',
 ]
 
 mysql_requires = [
