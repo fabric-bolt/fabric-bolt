03808d5ffb268179da832d21db44555343cfa1b2
Jared Proffitt <jproffitt@worthwhile.com>
2015-08-12 20:12:58 -0500

    test fixes

--- fabric_bolt/manage.py
+++ fabric_bolt/manage.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fabric_bolt.core.settings.local')
     sys.path.append(os.getcwd())
 
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
     from django.conf import settings
 
     if getattr(settings, 'SOCKETIO_ENABLED', False):
         from gevent import monkey
         monkey.patch_all()
-
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
