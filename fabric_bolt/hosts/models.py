57885f400be12aa1e169b703e37faf7dc9a1a512
Nathaniel Pardington <npardington@gmail.com>
2015-09-10 10:51:21 -0500

    Move unused validator off to a mixin

--- fabric_bolt/hosts/models.py
+++ fabric_bolt/hosts/models.py
@@ -1,19 +1,8 @@
 import re
 
 from django.db import models
-from django.core.validators import URLValidator, ValidationError
+from django.core.validators import ValidationError
 from django.utils.translation import ugettext_lazy as _
-
-
-class SchemelessURLValidator(URLValidator):
-    """Old valitador, keeping to migrations work"""
-    regex = re.compile(
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-    r'localhost|'  # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
-    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
 def full_domain_validator(hostname):
