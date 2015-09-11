cd9d9005155deb0cfefe366b65b93ec49b17a38d
Nathaniel Pardington <npardington@gmail.com>
2015-09-11 11:39:16 -0500

    Combine migrations

--- fabric_bolt/hosts/migrations/0002_sshconfig.py
+++ fabric_bolt/hosts/migrations/0002_sshconfig.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import fabric_bolt.core.mixins.storages
 
 
 class Migration(migrations.Migration):
@@ -17,7 +18,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=255)),
                 ('public_key', models.TextField()),
-                ('private_key_file', models.CharField(max_length=1500)),
+                ('private_key_file', models.FileField(storage=fabric_bolt.core.mixins.storages.FileStorageCHMOD600(), upload_to=b'private_keys')),
+                ('remote_user', models.CharField(default=b'root', max_length=100)),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
             ],
         ),
